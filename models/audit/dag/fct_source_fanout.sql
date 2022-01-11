2c5672a2fab40922c01ac99233691916f8a410f2
graciegoheen <graciegoheen@gmail.com>
2022-01-11 13:53:51 -0500

    changed macro into model and built first fct_ model to check for dag issues

---
+++ models/audit/dag/fct_source_fanout.sql
@@ -0,0 +1,28 @@
+-- this model finds cases where a source is used in multiple direct downstream models
+with direct_source_relationships as (
+    select  
+        *
+    from {{ ref('stg_dag_relationships') }}
+    where distance = 1
+    and parent_type = 'source'
+),
+
+source_fanout as (
+    select
+        parent,
+        count(*)
+    from direct_source_relationships
+    group by 1
+    having count(*) > 1
+),
+
+final as (
+    select 
+        direct_source_relationships.*
+    from direct_source_relationships
+    inner join source_fanout
+    on direct_source_relationships.parent = source_fanout.parent
+    order by direct_source_relationships.parent
+)
+
+select * from final
