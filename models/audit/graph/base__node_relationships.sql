97bd2cc81c7ee9f345da895a595ad1c9ee8e0f21
graciegoheen <graciegoheen@gmail.com>
2022-02-01 12:36:17 -0500

    changed strategy to select * from values

--- models/audit/graph/base__node_relationships.sql
+++ models/audit/graph/base__node_relationships.sql
@@ -1,3 +1,28 @@
-{{
-    get_node_relationships()
-}}
+select * from ( values
+
+    {% set nodes_list = nodes_list %}
+    
+    {%- for node in graph.nodes.values() -%}
+    {%- set outer_loop = loop -%}
+
+        {%- if node.depends_on.nodes|length == 0 -%}
+
+            ('{{ node.name }}','{{ node.unique_id }}','{{ node.resource_type }}',NULL)
+
+        {%- else -%}       
+
+            {%- for parent in node.depends_on.nodes -%}
+
+                ('{{ node.name }}','{{ node.unique_id }}','{{ node.resource_type }}','{{ parent }}'){% if not loop.last %},{% endif %}
+
+            {% endfor -%}
+        
+        {%- endif %}
+
+        {% if not outer_loop.last %},{% endif %}
+
+    {% endfor -%}
+
+)
+
+-- likely want to move above code to macro
