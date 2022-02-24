76068f98b0723492232aad53086f3228633c3515
Benoit Perigaud <benoit.perigaud@dbtlabs.com>
2022-02-24 20:35:35 +0100

    Add metrics to the DAG

---
+++ models/audit/graph/base__metric_relationships.sql
@@ -0,0 +1,17 @@
+with relationships as (
+
+    {{
+        get_relationships("metrics")
+    }}
+
+),
+
+
+final as (
+    select 
+        {{ dbt_utils.surrogate_key(['node_id', 'direct_parent_id']) }} as unique_id, 
+        *
+    from relationships
+)
+
+select distinct * from final
