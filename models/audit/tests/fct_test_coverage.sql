22b9c8ccea72515fa694081f9d17b3c12fad728e
Benoit Perigaud <benoit.perigaud@dbtlabs.com>
2022-03-15 15:02:11 +0100

    Move the 100 before to support division as integer

--- models/audit/tests/fct_test_coverage.sql
+++ models/audit/tests/fct_test_coverage.sql
@@ -10,7 +10,7 @@ final as (
         count(*) as total_models,
         sum(tests_per_model) as total_tests,
         sum(case when tests_per_model > 0 then 1 else 0 end) as tested_models,
-        round(sum(case when tests_per_model > 0 then 1 else 0 end) / count(*), 4) * 100 as test_coverage_pct, 
+        round(sum(case when tests_per_model > 0 then 1 else 0 end) * 100 / count(*), 4) as test_coverage_pct, 
         round(sum(tests_per_model) / count(*), 4)  as test_to_model_ratio 
     from test_counts
 )
