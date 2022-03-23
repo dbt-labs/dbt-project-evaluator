d18c535ef47dc203778f7af07e35234b14e5d1e1
Grace Goheen <graciegoheen@gmail.com>
2022-03-23 14:55:45 -0400

    Added sources to fct_staging_directories

--- models/structure/fct_staging_directories.sql
+++ models/structure/fct_staging_directories.sql
@@ -1,37 +1,67 @@
 -- Because we often work with multiple data sources, in our staging directory, we create one directory per source.
 
--- This model finds all cases where a staging model is NOT in the appropriate subdirectory
+-- This model finds all cases where a staging model or source definition is NOT in the appropriate subdirectory
     -- how should we define "staging" model here? 
         -- by naming convention in stg_all_graph_resource?
         -- by being in a folder called staging?
         -- or all direct children of source tables?
 
-with staging_relationships as (
-    select * from {{ ref('int_all_dag_relationships') }}
-    where child_resource_type = 'model'
-    and child_model_type = 'staging'
+-- TO DO: consider also adding tests/documentation that are in the incorrect subdirectory?
+
+with all_graph_resources as (
+    select * from {{ ref('stg_all_graph_resources') }}
 ),
 
-source_parents as (
+all_dag_relationships as (
+    select * from {{ ref('int_all_dag_relationships') }}
+),
+
+staging_models as (
     select  
         child,
         child_file_path,
         -- should we create a new field called source_name in stg_all_graph_resources and bring through so we don't have to do a this substring?
         left(parent, charindex('.',parent) - 1) as source,
-        left(child_file_path, len(child_file_path) - charindex('/',reverse(child_file_path)) + 1) as child_directory_path
-    from staging_relationships
+        left(child_file_path, len(child_file_path) - charindex('/',reverse(child_file_path)) + 1) as child_directory_path,
+        right(child_file_path, charindex('/', reverse(child_file_path)) - 1) as child_file_name
+    from all_dag_relationships
     where parent_resource_type = 'source'
+    and child_resource_type = 'model'
+    and child_model_type = 'staging'
 ),
 
 -- find all staging models that are NOT in their source parent's subdirectory
 inappropriate_subdirectories_staging as (
     select 
-        child as model,
-        child_directory_path as current_directory_path,
-        'models/staging/' || source || '/' as change_directory_path_to
-    from source_parents
+        child as resource_name,
+        child_file_path as current_file_path,
+        'models/staging/' || source || '/' || child_file_name as change_file_path_to
+    from staging_models
     where child_directory_path not like '%' || source || '%'
-)
+),
+
+sources as (
+    select 
+        resource_name,
+        file_path,
+        left(resource_name, charindex('.',resource_name) - 1) as source,
+        left(file_path, len(file_path) - charindex('/',reverse(file_path)) + 1) as current_directory_path,
+        right(file_path, charindex('/', reverse(file_path)) - 1) as file_name
+    from all_graph_resources
+    where resource_type = 'source'
+),
 
 -- find all sources that are definied in a .yml file NOT in their subdirectory
+inappropriate_subdirectories_sources as (
+    select 
+        resource_name,
+        file_path as current_file_path,
+        'models/staging/' || source || '/' || file_name as change_file_path_to
+    from sources
+    where current_directory_path not like '%' || source || '%'
+)
+
+
 select * from inappropriate_subdirectories_staging
+union all 
+select * from inappropriate_subdirectories_sources
