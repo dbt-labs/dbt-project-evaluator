8bbfa1600516f72b1dbabedec421a688c99f5c98
wasilaq <wasila.quader@dbtlabs.com>
2022-04-07 10:20:41 -0400

    resolve merge conflict (remoce my changes)

--- models/staging/graph/stg_all_graph_resources.sql
+++ models/staging/graph/stg_all_graph_resources.sql
@@ -13,52 +13,52 @@ with unioned as (
 final as (
 
     select
-        unique_id as resource_id, 
-        case 
+        unique_id as resource_id,
+        case
             when resource_type = 'source' then source_name || '.' || name
-            else name 
-        end as resource_name, 
-        resource_type, 
-        file_path, 
-        case 
+            else name
+        end as resource_name,
+        resource_type,
+        file_path,
+        case
             when resource_type in ('test', 'source', 'metric', 'exposure') then null
-            when file_path like '%{{ var("staging_folder_name") }}%' 
+            when file_path like '%{{ var("staging_folder_name") }}%'
                 {%- for prefix in var("staging_prefixes") -%}
                 or name like '%{{ prefix }}%'
                 {% endfor %}
                 then 'staging'
-            when file_path like '%{{ var("intermediate_folder_name") }}%' 
+            when file_path like '%{{ var("intermediate_folder_name") }}%'
                 {%- for prefix in var("intermediate_prefixes") -%}
                 or name like '%{{ prefix }}%'
                 {% endfor %}
                 then 'intermediate'
-            when file_path like '%{{ var("marts_folder_name") }}%' 
+            when file_path like '%{{ var("marts_folder_name") }}%'
                 {%- for prefix in var("marts_prefixes") -%}
                 or name like '%{{ prefix }}%'
                 {% endfor %}
                 then 'marts'
             else 'other' -- is this the catch-all that we want? what about the reports folder in our example DAG?
-        end as model_type, 
-        is_enabled, 
-        materialized, 
-        on_schema_change, 
-        database, 
+        end as model_type,
+        is_enabled,
+        materialized,
+        on_schema_change,
+        database,
         schema, 
-        package_name, 
-        alias, 
-        is_described, 
-        exposure_type, 
-        maturity, 
-        url, 
-        metric_type, 
-        model, 
-        label, 
-        sql, 
-        timestamp as timestamp,  
+        package_name,
+        alias,
+        is_described,
+        exposure_type,
+        maturity,
+        url,
+        metric_type,
+        model,
+        label,
+        sql,
+        timestamp as timestamp,
         source_name, -- NULL for non-source resources
-        is_source_described, 
-        loaded_at_field, 
-        loader, 
+        is_source_described,
+        loaded_at_field,
+        loader,
         identifier
 
     from unioned
