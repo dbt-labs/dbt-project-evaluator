3cf43ef8f46e12f01a7e9ced19f42103078fe6a3
Dave Connors <dave.connors@fishtownanalytics.com>
2022-04-20 16:11:40 -0500

    add column level logic

--- integration_tests/models/staging/source_1/stg_model_2.sql
+++ integration_tests/models/staging/source_1/stg_model_2.sql
@@ -1,2 +1,4 @@
-{{ source('source_1', 'table_1') }}
-{{ source('source_1', 'table_2') }}
+-- depends on: {{ source('source_1', 'table_1') }}
+-- depends on: {{ source('source_1', 'table_2') }}
+
+select 1 as id
