6418d4da4c410d5e3308a99dfbbbdf16188fb028
Grace Goheen <graciegoheen@gmail.com>
2022-04-22 11:24:06 -0400

    Added _ to end of prefixes

--- models/structure/fct_model_naming_conventions.sql
+++ models/structure/fct_model_naming_conventions.sql
@@ -10,12 +10,13 @@ with all_graph_resources as (
 models as (
     select
         resource_name,
-        {{ dbt_utils.split_part('resource_name', "'_'", 1) }} as prefix,
+        {{ dbt_utils.split_part('resource_name', "'_'", 1) }}||'_' as prefix,
         model_type,
         case 
             when model_type = 'staging' then '{{ var("staging_prefixes") | join(", ") }}'
             when model_type = 'intermediate' then '{{ var("intermediate_prefixes") | join(", ") }}'
             when model_type = 'marts' then '{{ var("marts_prefixes") | join(", ") }}'
+            when model_type = 'other' then '{{ var("other_prefixes") | join(", ") }}'
             else null -- TO DO: how do we handle additional model types? 
         end as appropriate_prefixes
     from all_graph_resources 
@@ -25,8 +26,9 @@ models as (
 inappropriate_model_names as (
     select 
         resource_name,
+        prefix,
         model_type,
-        appropriate_prefixes     
+        appropriate_prefixes
     from models
     where coalesce( {{ dbt_utils.position('prefix', 'appropriate_prefixes') }}, 0) = 0
 
