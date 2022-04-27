adc6785f69d1b60ba5940bc7c4fd2eb2757667fa
Grace Goheen <graciegoheen@gmail.com>
2022-04-27 19:24:38 -0400

    removed to do comments from fct_root_models

--- models/marts/dag/fct_root_models.sql
+++ models/marts/dag/fct_root_models.sql
@@ -1,6 +1,4 @@
 -- this model finds cases where a model has 0 direct parents, likely due to a lack of source or ref function
--- TO DO: 
-    -- decide how seeds are handled in these dag tests, seeds are in the base__nodes model where resource_type = 'seed' 
 
 with model_relationships as (
     select  
@@ -18,23 +16,3 @@ final as (
 )
 
 select * from final
-
--- TO DO: Which query is better?
-
--- with direct_relationships as ( 
---     select  
---         *
---     from int_direct_relationships
--- ),
--- 
--- final as (
---     select distinct 
---         node,
---         resource_type,
---         direct_parent_id
---     from direct_relationships
---     where resource_type = 'model'
---     and direct_parent_id is NULL
--- )
--- 
--- select * from final
