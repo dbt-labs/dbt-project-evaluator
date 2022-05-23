e3a4dad6343aca09998c2f8305f711f361953f1e
Dave Connors <dave.connors@fishtownanalytics.com>
2022-05-23 11:42:05 -0500

    add docs to all models that need it

--- models/staging/graph/stg_naming_convention_prefixes.sql
+++ models/staging/graph/stg_naming_convention_prefixes.sql
@@ -2,10 +2,25 @@
 
 with vars_prefix_table as (
     {{ loop_vars(vars_prefix) }}
-)
+),
+
+parsed as (
 
 select
     var_name as prefix_name, 
     {{ dbt_utils.split_part('var_name', "'_'", 1) }} as model_type,
     var_value as prefix_value
 from vars_prefix_table
+
+),
+
+final as (
+
+    select
+        {{ dbt_utils.surrogate_key(['model_type', 'prefix_value']) }} as unique_id,
+        *
+    from parsed
+
+)
+
+select * from final
