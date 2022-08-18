7d9d593d7329c58f9b12b4c923e9c455ad60b472
Benoit Perigaud <8754100+b-per@users.noreply.github.com>
2022-08-18 14:39:32 +0200

    Fix CI issue

--- run_test.sh
+++ run_test.sh
@@ -23,5 +23,6 @@ dbt build -x --target $1 --full-refresh || exit 1
 
 # test with the second project
 cd ../integration_tests_2
+dbt deps --target $1 || exit 1
 dbt seed --full-refresh --target $1 || exit 1
 dbt run -x --target $1 --full-refresh || exit 1
