9a00333ab822b7568b9bdb49b870d40156c578d7
Dave Connors <dave.connors@fishtownanalytics.com>
2022-09-06 08:26:26 -0500

    handle nulls

--- macros/wrap_string_with_quotes.sql
+++ macros/wrap_string_with_quotes.sql
@@ -1,6 +1,6 @@
 {% macro wrap_string_with_quotes(str) %}
   {% if not str %}
-    {{ return('NULL') }}
+    {{ return('cast(NULL as ' ~ dbt_utils.type_string() ~ ')') }}
   {% else %}
     {{ return("'" ~ str ~ "'") }}
   {% endif %}
