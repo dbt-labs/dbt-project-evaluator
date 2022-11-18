608bbd80a7817a6df978eef049d3a3de0f6154be
jschintz-windriver <112569796+jschintz-windriver@users.noreply.github.com>
2022-11-18 09:32:40 -0500

    Update get_exposures.sql

--- macros/unpack/get_exposures.sql
+++ macros/unpack/get_exposures.sql
@@ -9,7 +9,7 @@
         {%- set nodes_list = graph.exposures.values() -%}
         {%- set values = [] -%}
         
-    {{ get_paths(dbt_project_evaluator_path_pattern='(^marts)|(^staging)|(^intermediate)') }}
+        {%- set paths = get_paths(dbt_project_evaluator_path_pattern='(^marts)|(^staging)|(^intermediate)') -%}
           
         {%- for node in nodes_list
           | selectattr("path", "in", paths) -%}
