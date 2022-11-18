d73f585b90f61649551b0f6a97fe1195aa8e4e38
jschintz-windriver <112569796+jschintz-windriver@users.noreply.github.com>
2022-11-18 10:51:52 -0500

    Update get_paths.sql

--- macros/unpack/get_paths.sql
+++ macros/unpack/get_paths.sql
@@ -1,10 +1,10 @@
-{%- macro get_paths(dbt_project_evaluator_path_pattern) -%}
+{%- macro get_paths(path_pattern) -%}
   {% if execute %} 
   {# Get all paths for nodes that match a regex pattern #}
     {% set paths = [] -%}
     {# Is just the nodes graph enough? #}
     {% for path in graph.nodes.values() -%}
-      {%- if modules.re.match(dbt_project_evaluator_path_pattern, path.path) is not none -%} {%-do paths.append(path.path) -%} {% endif %}
+      {%- if modules.re.match(path_pattern, path.path) is not none -%} {%-do paths.append(path.path) -%} {% endif %}
     {%- endfor %}
   {% endif %}
   {{ return(paths) }}
