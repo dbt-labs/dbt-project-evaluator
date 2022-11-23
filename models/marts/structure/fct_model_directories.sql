fb655550bad204036da315d24ce9647580636cee
angelica-lastra <angelica.lastra@dbtlabs.com>
2022-11-23 11:34:58 -0700

    updated code to use directory pattern macro

--- models/marts/structure/fct_model_directories.sql
+++ models/marts/structure/fct_model_directories.sql
@@ -36,7 +36,7 @@ inappropriate_subdirectories_staging as (
         child_resource_type as resource_type,
         child_model_type as model_type,
         child_file_path as current_file_path,
-        'models/' || '{{ var("staging_folder_name") }}' || '/' || parent_source_name || '/' || child_file_name as change_file_path_to
+        'models{{directory_pattern()}}' || '{{ var("staging_folder_name") }}' || '{{directory_pattern()}}' || parent_source_name || '{{directory_pattern()}}' || child_file_name as change_file_path_to
     from staging_models
     where child_directory_path not like '%' || parent_source_name || '%'
 ),
@@ -48,7 +48,7 @@ innappropriate_subdirectories_non_staging_models as (
         all_graph_resources.resource_type,
         all_graph_resources.model_type,
         all_graph_resources.file_path as current_file_path,
-        'models' || '/.../' || folders.folder_name_value || '/.../' || all_graph_resources.file_name as change_file_path_to
+        'models' || '{{directory_pattern()}}...{{directory_pattern()}}' || folders.folder_name_value || '{{directory_pattern()}}...{{directory_pattern()}}' || all_graph_resources.file_name as change_file_path_to
     from all_graph_resources
     left join folders 
         on folders.model_type = all_graph_resources.model_type 
