74f7dc26f03ccd4063b789bb0aa94cda40c4feb9
angelica-lastra <angelica.lastra@dbtlabs.com>
2022-11-23 11:48:03 -0700

    added description to macro

--- macros/directory_pattern.sql
+++ macros/directory_pattern.sql
@@ -1,3 +1,4 @@
+-- these macros will read a user’s home environment and detect whether a computer’s operating system is Windows based or Mac/Linux, and display the right directory pattern.
 {% macro directory_pattern() %}
   {%- if env_var("HOME", "not_set") != "not_set" -%}
      {% if "\\\\" not in env_var("HOME", "not_set") %}
