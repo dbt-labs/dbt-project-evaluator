86e6a57a1d0b5733c666fdc44ca48eef2d05a12a
jschintz-windriver <112569796+jschintz-windriver@users.noreply.github.com>
2022-11-30 15:54:03 -0500

    Update get_paths.sql

--- macros/unpack/get_paths.sql
+++ macros/unpack/get_paths.sql
@@ -2,8 +2,11 @@
   {% if execute %} 
   {# Get all paths for nodes that match a regex pattern #}
     {% set paths = [] -%}
-    {# Is just the nodes graph enough? #}
+    {# Is just the nodes & sources graph enough? #}
     {% for path in graph.nodes.values() -%}
+      {%- if modules.re.match(path_pattern, path.original_file_path) is not none -%} {%-do paths.append(path.original_file_path) -%} {% endif %}
+    {%- endfor %}
+    {% for path in graph.sources.values() -%}
       {%- if modules.re.match(path_pattern, path.original_file_path) is not none -%} {%-do paths.append(path.original_file_path) -%} {% endif %}
     {%- endfor %}
   {% endif %}
