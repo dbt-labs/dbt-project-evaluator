7d223ec4bc5021e5032bbf6550b9edf5158f652f
Grace Goheen <graciegoheen@gmail.com>
2022-12-20 14:27:39 -0600

    added extra trim because escape_single_quotes is adding extra whitespace

--- macros/unpack/get_nodes.sql
+++ macros/unpack/get_nodes.sql
@@ -28,7 +28,7 @@
                 "cast(" ~ dbt_project_evaluator.is_not_empty_string(node.description) | trim ~ " as boolean)",
                 "''" if not node.column_name else wrap_string_with_quotes(dbt.escape_single_quotes(node.column_name)),
                 wrap_string_with_quotes(node.meta | tojson),
-                wrap_string_with_quotes(dbt.escape_single_quotes(hard_coded_references)),
+                wrap_string_with_quotes(dbt.escape_single_quotes(hard_coded_references) | trim),
                 wrap_string_with_quotes(node.get('depends_on',{}).get('macros',[]) | tojson),
                 "cast(" ~ dbt_project_evaluator.is_not_empty_string(node.test_metadata) | trim ~ " as boolean)"
             ]
