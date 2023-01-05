2b917303b95593962e624d1470348971a1e9913b
Dave Connors <dave.connors@fishtownanalytics.com>
2023-01-05 16:57:53 -0600

    remove old macros

--- models/staging/graph/base/base_metric_relationships.sql
+++ models/staging/graph/base/base_metric_relationships.sql
@@ -12,7 +12,6 @@ with dummy_cte as (
 ) 
 
 select 
-
     cast(null as {{ dbt.type_string()}}) as resource_id,
     cast(null as {{ dbt.type_string()}}) as direct_parent_id,
     cast(True as boolean) as is_primary_relationship
