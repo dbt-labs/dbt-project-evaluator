0abfdbaaade568aeabcf4eccdc0c11e4ede40d1f
Grace Goheen <graciegoheen@gmail.com>
2023-01-05 13:31:25 -0700

    check if seed exists in warehouse before filtering

--- macros/filter_exceptions.sql
+++ macros/filter_exceptions.sql
@@ -1,22 +1,28 @@
 {% macro filter_exceptions(model_ref) -%}
-  {{ return(adapter.dispatch('filter_exceptions', 'dbt_project_evaluator')(model_ref)) }}
+    {{ return(adapter.dispatch('filter_exceptions', 'dbt_project_evaluator')(model_ref)) }}
 {%- endmacro %}
 
 {% macro default__filter_exceptions(model_ref) %}
 
-{% set query_filters %}
-select
-    column_name,
-    id_to_exclude
-from {{ ref('dbt_project_evaluator_exceptions') }}
-where fct_name = '{{ model_ref.name }}'
-{% endset %}
+    {% set seed_exists = load_relation(ref('dbt_project_evaluator_exceptions')) is not none %}
 
-{% if execute and flags.WHICH not in ['compile'] %}
-    where 1 = 1
-    {% for row_filter in run_query(query_filters) %}
-        and {{ row_filter[0] }} not like '{{ row_filter[1] }}'
-    {% endfor %}
-{% endif %}
+    {% if seed_exists %}
+
+        {% set query_filters %}
+        select
+            column_name,
+            id_to_exclude
+        from {{ ref('dbt_project_evaluator_exceptions') }}
+        where fct_name = '{{ model_ref.name }}'
+        {% endset %}
+    
+        {% if execute and flags.WHICH not in ['compile'] %}
+            where 1 = 1
+            {% for row_filter in run_query(query_filters) %}
+                and {{ row_filter[0] }} not like '{{ row_filter[1] }}'
+            {% endfor %}
+        {% endif %}
+    
+    {% endif %}
 
 {% endmacro %}
