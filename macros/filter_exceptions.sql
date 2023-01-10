7a3e4bec314ad88a1bd82cb384133b1152daf531
sadahry <ce01015@p.cartahd.com>
2023-01-10 11:50:48 +0900

    enable to use '%' in column_name

--- macros/filter_exceptions.sql
+++ macros/filter_exceptions.sql
@@ -15,7 +15,16 @@ where '{{ model_ref.name }}' like fct_name
 {% if execute and flags.WHICH not in ['compile'] %}
     where 1 = 1
     {% for row_filter in run_query(query_filters) %}
-        and {{ row_filter[0] }} not like '{{ row_filter[1] }}'
+        {% if '%' in row_filter[0] %}
+            {% set column_pattern = row_filter[0] | replace('%', '.*') %}
+            {% for column in model_ref.columns %}
+                {% if modules.re.match(column_pattern, column.name) %}
+                    and {{ column.name }} not like '{{ row_filter[1] }}'
+                {% endif %}
+            {% endfor %}
+        {% else %}
+            and {{ row_filter[0] }} not like '{{ row_filter[1] }}'
+        {% endif %}
     {% endfor %}
 {% endif %}
 
