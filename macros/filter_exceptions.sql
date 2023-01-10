bf996b194a0fab879ea6e1d95183c14012bdd536
sadahry <ce01015@p.cartahd.com>
2023-01-10 12:17:32 +0900

    fix error of getting columns

--- macros/filter_exceptions.sql
+++ macros/filter_exceptions.sql
@@ -12,15 +12,15 @@ from {{ ref('dbt_project_evaluator_exceptions') }}
 where '{{ model_ref.name }}' like fct_name
 {% endset %}
 
+{% set all_columns = adapter.get_columns_in_relation(model_ref) %}
+
 {% if execute and flags.WHICH not in ['compile'] %}
     where 1 = 1
     {% for row_filter in run_query(query_filters) %}
         {% if '%' in row_filter[0] %}
             {% set column_pattern = row_filter[0] | replace('%', '.*') %}
-            {% for column in model_ref.columns %}
-                {% if modules.re.match(column_pattern, column.name) %}
-                    and {{ column.name }} not like '{{ row_filter[1] }}'
-                {% endif %}
+            {% for column in all_columns if modules.re.match(column_pattern, column.name) %}
+                and {{ column.name }} not like '{{ row_filter[1] }}'
             {% endfor %}
         {% else %}
             and {{ row_filter[0] }} not like '{{ row_filter[1] }}'
