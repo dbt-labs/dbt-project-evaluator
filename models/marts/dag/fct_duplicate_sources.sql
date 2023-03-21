756d380b6a00d911689127dec82c29aa85634b0e
Grace Goheen <125407480+ggoheenliveramp@users.noreply.github.com>
2023-03-21 16:31:07 -0700

    add new fct_duplicate_sources model and integration test

---
+++ models/marts/dag/fct_duplicate_sources.sql
@@ -0,0 +1,22 @@
+with sources as (
+    select
+        resource_name,
+        {{ dbt.concat(["database", "'.'", "schema", "'.'", "identifier"]) }} as source_db_location 
+    from {{ ref('int_all_graph_resources') }}
+    where resource_type = 'source'
+),
+
+source_duplicates as (
+    select
+        source_db_location,
+        {{ dbt.listagg(
+            measure = 'resource_name', 
+            delimiter_text = "', '", 
+            order_by_clause = 'order by resource_name' if target.type in ['snowflake','redshift','duckdb']) 
+        }} as source_names
+    from sources
+    group by source_db_location
+    having count(*) > 1
+)
+
+select * from source_duplicates
