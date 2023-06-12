01de29db6e0c4cf4f21a3096ced86e3031ae4ddb
Benoit Perigaud <8754100+b-per@users.noreply.github.com>
2023-06-12 12:37:16 +0200

    Update tests depending if they run on 1.5 or later

--- run_test.sh
+++ run_test.sh
@@ -19,12 +19,17 @@ fi
 . $VENV
 
 cd integration_tests
-
 if [[ ! -e ~/.dbt/profiles.yml ]]; then
     mkdir -p ~/.dbt
     cp ci/sample.profiles.yml ~/.dbt/profiles.yml
 fi
 
+# used for now while we still test against 1.5 on DuckdDB
+if [[ $1 == "duckdb" ]] ; then
+    cp models/marts/metrics.yml.dbt_metrics models/marts/metrics.yml
+fi
+
+# tests with the first project
 dbt deps --target $1 || exit 1
 dbt build -x --target $1 --full-refresh || exit 1
 
