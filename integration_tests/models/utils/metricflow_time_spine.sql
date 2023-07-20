e7b7bdf0ee85696deab5dbcbfa9881e55b57a96b
Dave Connors <dave.connors@fishtownanalytics.com>
2023-07-20 10:07:04 -0500

    add metricflow spine, adjsut count metrics, add exceptions to tests and global seed

---
+++ integration_tests/models/utils/metricflow_time_spine.sql
@@ -0,0 +1,22 @@
+{{
+  config(
+    materialized = 'table',
+    )
+}}
+
+-- metricflow_time_spine.sql
+with days as (
+    {{ dbt_utils.date_spine('day'
+    , "to_date('01/01/2000','mm/dd/yyyy')"
+    , "to_date('01/01/2027','mm/dd/yyyy')"
+    )
+    }}
+),
+
+final as (
+    select cast(date_day as date) as date_day
+    from days
+)
+
+select *
+from final
