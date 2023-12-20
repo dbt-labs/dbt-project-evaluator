a4c01ef9e2925498e44f73d53ce16c32b298b498
Benoit Perigaud <8754100+b-per@users.noreply.github.com>
2023-12-20 10:19:50 +0100

    Handle single quote in owner name and update test

--- macros/unpack/get_exposure_values.sql
+++ macros/unpack/get_exposure_values.sql
@@ -22,7 +22,7 @@
               wrap_string_with_quotes(node.maturity),
               wrap_string_with_quotes(node.package_name),
               wrap_string_with_quotes(node.url),
-              wrap_string_with_quotes(node.owner.name),
+              wrap_string_with_quotes(dbt.escape_single_quotes(node.owner.name)),
               wrap_string_with_quotes(node.owner.email),
               wrap_string_with_quotes(node.meta | tojson)
             ]
