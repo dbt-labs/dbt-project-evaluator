310ed8966d3bc9f7c41cb1843a31107ab6aada60
sadahry <ce01015@p.cartahd.com>
2023-12-21 10:32:10 +0900

    add test_name_prefix_of_custom_rules

--- macros/on-run-end/print_dbt_project_evaluator_issues.sql
+++ macros/on-run-end/print_dbt_project_evaluator_issues.sql
@@ -3,9 +3,24 @@
   {%- if flags.WHICH in ["build","test"] -%}
     {{ print("\n### List of issues raised by dbt_project_evaluator ###") }}
 
+    {#-
+      if you create custom dbt_project_evaluator rules on your package using the test `dbt_project_evaluator.is_empty`,
+      the test name should start with the same name as the default.
+    -#}
+    {% set test_name_prefix_of_custom_rules = var(
+      "test_name_prefix_of_custom_rules",
+      default="dbt_project_evaluator_is_empty_",
+    ) %}
+
     {% for result in results | selectattr('failures') | selectattr('failures', '>', 0) %}
       
-      {% if result.node.fqn[0] == "dbt_project_evaluator" %}
+      {% set is_test = result.node.config.materialized == "test" %}
+      {% set package_name = result.node.package_name %}
+      {% set resource_name = result.node.name %}
+      {% if is_test and (
+        package_name == "dbt_project_evaluator"
+        or resource_name.startswith(test_name_prefix_of_custom_rules)
+      ) %}
         
         {{ print("\n-- " ~ result.node.alias ~ " --") }}
 
