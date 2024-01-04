62feca5d1eb8a7f30bbbc303e026d56d61dce9ff
Grace Goheen <grace.goheen@dbtlabs.com>
2024-01-04 15:56:09 -0700

    change example model to prevent breaking other integration tests

--- integration_tests/models/staging/stg_model_5.sql
+++ integration_tests/models/staging/stg_model_5.sql
@@ -1,6 +1,3 @@
 select 
     * 
 from {{ ref('fct_model_9') }}
--- inner join {{ ref('fct_model_6') }}
--- on fct_model_9.join_field = fct_model_6.join_field
--- where fct_model_9.id is distinct from fct_model_6.id
