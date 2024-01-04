5f5319ce7b7519fd7b436d0c987bd89c98c9699d
Brad Cronkrite <151274228+BradCr@users.noreply.github.com>
2024-01-04 13:35:21 -0800

    Created fct_model_10.sql, which should trigger fct_number_of_joins to identify it

---
+++ integration_tests/models/marts/fct_model_10.sql
@@ -0,0 +1,66 @@
+{{
+  config(
+    materialized = 'table',
+    )
+}}
+
+with stg_model_1 as (
+    select * from {{ ref('stg_model_1') }}
+)
+
+,stg_model_2 as (
+    select * from {{ ref('stg_model_2') }}
+)
+
+,stg_model_3 as (
+    select * from {{ ref('stg_model_3') }}
+)
+
+,stg_model_4 as (
+    select * from {{ ref('stg_model_4') }}
+)
+
+,stg_model_5 as (
+    select * from {{ ref('stg_model_5') }}
+)
+
+,fct_model_6 as (
+    select * from {{ ref('fct_model_6') }}
+)
+
+,fct_model_9 as (
+    select * from {{ ref('stg_model_9') }}
+)
+
+,final as (
+    select
+        id
+    from stg_model_1
+    union all
+    select
+        id
+    from stg_model_2
+    union all
+    select
+        id
+    from stg_model_3
+    union all
+    select
+        id
+    from stg_model_4
+    union all
+    select
+        id
+    from stg_model_5
+    union all
+    select
+        id
+    from fct_model_6
+    union all
+    select
+        id
+    from fct_model_9
+    union all
+)
+
+select * from final
