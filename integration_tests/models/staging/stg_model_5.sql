bdd7defc9ecd5a65f8685ac86a7a9fdacda5c6ca
Grace Goheen <grace.goheen@dbtlabs.com>
2024-01-04 15:49:01 -0700

    add is distinct from example

--- integration_tests/models/staging/stg_model_5.sql
+++ integration_tests/models/staging/stg_model_5.sql
@@ -1,1 +1,6 @@
-{{ ref('fct_model_9') }}
+select 
+    * 
+from {{ ref('fct_model_9') }}
+-- inner join {{ ref('fct_model_6') }}
+-- on fct_model_9.join_field = fct_model_6.join_field
+-- where fct_model_9.id is distinct from fct_model_6.id
