28639294e4b8c1af2edf8078423696b5f47f4641
Serhii Dimchenko <svdimchenko@gmail.com>
2024-01-08 16:43:40 +0100

    Run integration tests for athena

--- macros/recursive_dag.sql
+++ macros/recursive_dag.sql
@@ -134,149 +134,6 @@ all_relationships (
 {% endmacro %}
 
 
-{% macro atena__recursive_dag() %}
-with recursive direct_relationships
-    (
-        unique_id,
-        resource_id,
-        resource_name,
-        resource_type,
-        file_path,
-        directory_path,
-        file_name,
-        model_type,
-        materialized,
-        is_public,
-        access,
-        source_name,
-        is_excluded,
-        direct_parent_id,
-        is_primary_test_relationship
-    ) as (
-    select
-        *
-    from {{ ref('int_direct_relationships') }}
-    where resource_type <> 'test'
-),
-
--- recursive CTE
--- one record for every resource and each of its downstream children (including itself)
-all_relationships (
-    parent_id,
-    parent,
-    parent_resource_type,
-    parent_model_type,
-    parent_materialized,
-    parent_access,
-    parent_is_public,
-    parent_source_name,
-    parent_file_path,
-    parent_directory_path,
-    parent_file_name,
-    parent_is_excluded,
-    child_id,
-    child,
-    child_resource_type,
-    child_model_type,
-    child_materialized,
-    child_access,
-    child_is_public,
-    child_source_name,
-    child_file_path,
-    child_directory_path,
-    child_file_name,
-    child_is_excluded,
-    distance,
-    path,
-    is_dependent_on_chain_of_views
-) as (
-    -- anchor
-    select distinct
-        resource_id as parent_id,
-        resource_name as parent,
-        resource_type as parent_resource_type,
-        model_type as parent_model_type,
-        materialized as parent_materialized,
-        access as parent_access,
-        is_public as parent_is_public,
-        source_name as parent_source_name,
-        file_path as parent_file_path,
-        directory_path as parent_directory_path,
-        file_name as parent_file_name,
-        is_excluded as parent_is_excluded,
-        resource_id as child_id,
-        resource_name as child,
-        resource_type as child_resource_type,
-        model_type as child_model_type,
-        materialized as child_materialized,
-        access as child_access,
-        is_public as child_is_public,
-        source_name as child_source_name,
-        file_path as child_file_path,
-        directory_path as child_directory_path,
-        file_name as child_file_name,
-        is_excluded as child_is_excluded,
-        0 as distance,
-        {{ dbt.array_construct(['resource_name']) }} as path,
-        cast(null as boolean) as is_dependent_on_chain_of_views
-
-    from direct_relationships
-
-    union all
-
-    -- recursive clause
-    select
-        all_relationships.parent_id as parent_id,
-        all_relationships.parent as parent,
-        all_relationships.parent_resource_type as parent_resource_type,
-        all_relationships.parent_model_type as parent_model_type,
-        all_relationships.parent_materialized as parent_materialized,
-        all_relationships.parent_access as parent_access,
-        all_relationships.parent_is_public as parent_is_public,
-        all_relationships.parent_source_name as parent_source_name,
-        all_relationships.parent_file_path as parent_file_path,
-        all_relationships.parent_directory_path as parent_directory_path,
-        all_relationships.parent_file_name as parent_file_name,
-        all_relationships.parent_is_excluded as parent_is_excluded,
-        direct_relationships.resource_id as child_id,
-        direct_relationships.resource_name as child,
-        direct_relationships.resource_type as child_resource_type,
-        direct_relationships.model_type as child_model_type,
-        direct_relationships.materialized as child_materialized,
-        direct_relationships.access as child_access,
-        direct_relationships.is_public as child_is_public,
-        direct_relationships.source_name as child_source_name,
-        direct_relationships.file_path as child_file_path,
-        direct_relationships.directory_path as child_directory_path,
-        direct_relationships.file_name as child_file_name,
-        direct_relationships.is_excluded as child_is_excluded,
-        all_relationships.distance+1 as distance,
-        {{ dbt.array_append('all_relationships.path', 'direct_relationships.resource_name') }} as path,
-        case
-            when
-                all_relationships.child_materialized in ('view', 'ephemeral')
-                and coalesce(all_relationships.is_dependent_on_chain_of_views, true)
-                then true
-            else false
-        end as is_dependent_on_chain_of_views
-
-    from direct_relationships
-    inner join all_relationships
-        on all_relationships.child_id = direct_relationships.direct_parent_id
-
-    {% if var('max_depth_dag') | int > 0 %}
-        {% if var('max_depth_dag') | int < 2 or var('max_depth_dag') | int < var('chained_views_threshold') | int %}
-            {% do exceptions.raise_compiler_error(
-                'Variable max_depth_dag must be at least 2 and must be greater or equal to than chained_views_threshold.'
-                ) %}
-        {% else %}
-        where distance <= {{ var('max_depth_dag')}}
-        {% endif %}
-    {% endif %}
-)
-{% endmacro %}
-
-
 {% macro bigquery__recursive_dag() %}
 
 -- as of Feb 2022 BigQuery doesn't support with recursive in the same way as other DWs
@@ -408,6 +265,10 @@ with direct_relationships as (
 
 {% macro trino__recursive_dag() %}
 {#-- Although Trino supports a recursive WITH-queries,
--- it is less performant than creating CTEs with loops and unioning them --#}
+-- it is less performant than creating CTEs with loops and union them --#}
+    {{ return(bigquery__recursive_dag()) }}
+{% endmacro %}
+
+{% macro athena__recursive_dag() %}
     {{ return(bigquery__recursive_dag()) }}
 {% endmacro %}
