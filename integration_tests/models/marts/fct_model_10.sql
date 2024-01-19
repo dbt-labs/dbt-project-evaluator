77d7530a007730bb4cf1bc64a2c48a1df0ba74fd
Brad Cronkrite <151274228+BradCr@users.noreply.github.com>
2024-01-18 19:44:45 -0800

    Fixed fct_model_10 joins

--- integration_tests/models/marts/fct_model_10.sql
+++ integration_tests/models/marts/fct_model_10.sql
@@ -20,16 +20,16 @@ with stg_model_1 as (
     select * from {{ ref('stg_model_4') }}
 )
 
-,stg_model_5 as (
-    select * from {{ ref('stg_model_5') }}
-)
-
 ,fct_model_6 as (
     select * from {{ ref('fct_model_6') }}
 )
 
-,fct_model_9 as (
-    select * from {{ ref('fct_model_9') }}
+,int_model_4 as (
+    select * from {{ ref('int_model_4') }}
+)
+
+,int_model_5 as (
+    select * from {{ ref('int_model_5') }}
 )
 
 ,final as (
@@ -51,16 +51,15 @@ with stg_model_1 as (
     union all
     select
         id
-    from stg_model_5
-    union all
-    select
-        id
     from fct_model_6
     union all
     select
         id
-    from fct_model_9
+    from int_model_4
     union all
+    select
+        id
+    from int_model_5
 )
 
 select * from final
