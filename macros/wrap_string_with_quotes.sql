5d6396c3bf702bf14dc364b0a54b15246cc94e54
katieclaiborne <46941959+katieclaiborne@users.noreply.github.com>
2024-02-04 13:17:28 -0600

    update macro for bigquery adapter only

--- macros/wrap_string_with_quotes.sql
+++ macros/wrap_string_with_quotes.sql
@@ -1,4 +1,16 @@
-{% macro wrap_string_with_quotes(str) %}
+{% macro wrap_string_with_quotes() %}
+    {{ return(adapter.dispatch('wrap_string_with_quotes', 'dbt_project_evaluator')()) }}
+{% endmacro %}
+
+{% macro default__wrap_string_with_quotes(str) %}
+  {% if not str %}
+    {{ return('cast(NULL as ' ~ dbt.type_string() ~ ')') }}
+  {% else %}
+    {{ return("'" ~ str ~ "'") }}
+  {% endif %}
+{% endmacro %}
+
+{% macro bigquery__wrap_string_with_quotes(str) %}
   {% if not str %}
     {{ return('cast(NULL as ' ~ dbt.type_string() ~ ')') }}
   {% else %}
