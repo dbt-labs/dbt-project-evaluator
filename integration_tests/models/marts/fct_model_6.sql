f3fa8b4e252bd5271e0fdca639886a387b71e461
Benoit Perigaud <8754100+b-per@users.noreply.github.com>
2024-04-04 18:51:53 +0200

    Avoid SQL line comments when searching hard coded tables

--- integration_tests/models/marts/fct_model_6.sql
+++ integration_tests/models/marts/fct_model_6.sql
@@ -5,86 +5,89 @@
 }}
 
 select 1 as id 
--- from {{ ref('stg_model_3') }}
 
--- union all 
--- select
---     3 as id
--- from my_db.my_schema.my_table
--- union all 
--- select
---     3 as id
--- from 'my_db'.'my_schema'.'my_table'
--- union all 
--- select
---     3 as id
--- from "my_db"."my_schema"."my_table"
--- union all 
--- select
---     3 as id
--- from `my_db`.`my_schema`.`my_table`
--- union all 
--- select
---     3 as id
--- from [my_db].[my_schema].[my_table]
+-- depends on {{ ref('stg_model_3') }} 
 
--- union all
--- select 
---     4 as id
--- from my_schema.raw_relation_5
--- union all
--- select 
---     4 as id
--- from 'my_schema'.'raw_relation_5' 
--- union all
--- select 
---     4 as id
--- from "my_schema"."raw_relation_5"
--- union all
--- select 
---     4 as id
--- from `my_schema`.`raw_relation_5`
--- union all
--- select 
---     4 as id
--- from [my_schema].[raw_relation_5] 
+{#
+  from {{ ref('stg_model_3') }}   
+  union all 
+  select
+      3 as id
+  from my_db.my_schema.my_table
+  union all 
+  select
+      3 as id
+  from 'my_db'.'my_schema'.'my_table'
+  union all 
+  select
+      3 as id
+  from "my_db"."my_schema"."my_table"
+  union all 
+  select
+      3 as id
+  from `my_db`.`my_schema`.`my_table`
+  union all 
+  select
+      3 as id
+  from [my_db].[my_schema].[my_table]   
+  union all
+  select 
+      4 as id
+  from my_schema.raw_relation_5
+  union all
+  select 
+      4 as id
+  from 'my_schema'.'raw_relation_5' 
+  union all
+  select 
+      4 as id
+  from "my_schema"."raw_relation_5"
+  union all
+  select 
+      4 as id
+  from `my_schema`.`raw_relation_5`
+  union all
+  select 
+      4 as id
+  from [my_schema].[raw_relation_5]    
+  union all
+  select 
+      4 as id
+  from `raw_relation_1` 
+  union all
+  select 
+      4 as id
+  from "raw_relation_2" 
+  union all
+  select 
+      4 as id
+  from [raw_relation_3]  
+  union all
+  select 
+      4 as id
+  from 'raw_relation_4'    
+  union all
+  select
+      4 as id
+  from {{ var("my_table_reference") }}
+  union all
+  select
+      4 as id
+  from {{ var('my_table_reference') }}   
+  union all
+  
+  -- the following is SQL commented so it should be found as a hard coded reference
+--   select
+--       6 as id
+--   from my_db.my_schema.my_table222
+  
+  select
+      5 as id
+  from {{ var("my_table_reference", "table_d") }}
+  union all
+  select
+      5 as id
+  from {{ var('my_table_reference', 'table_d') }}
 
 
--- union all
--- select 
---     4 as id
--- from `raw_relation_1` 
--- union all
--- select 
---     4 as id
--- from "raw_relation_2" 
--- union all
--- select 
---     4 as id
--- from [raw_relation_3]  
--- union all
--- select 
---     4 as id
--- from 'raw_relation_4' 
-
--- union all
--- select
---     4 as id
--- from {{ var("my_table_reference") }}
--- union all
--- select
---     4 as id
--- from {{ var('my_table_reference') }}
-
-
--- union all
--- select
---     5 as id
--- from {{ var("my_table_reference", "table_d") }}
--- union all
--- select
---     5 as id
--- from {{ var('my_table_reference', 'table_d') }}
--- select
---     7 as id
--- from {{ var('my_table_reference', 'table_d') }}
+#}
