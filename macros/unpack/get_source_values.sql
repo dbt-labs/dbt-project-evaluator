08f56aad0720c283b09c6c0c5d99e740a34e000b
Alexandra Fetterman <alexandra.fetterman@instructure.com>
2024-04-12 13:54:59 -0700

    revert macro formatting

--- macros/unpack/get_source_values.sql
+++ macros/unpack/get_source_values.sql
@@ -12,29 +12,29 @@
 
         {%- set exclude_source = dbt_project_evaluator.set_is_excluded(node, resource_type="source") -%}
 
-        {%- set values_line = 
+         {%- set values_line = 
             [
-                wrap_string_with_quotes(node.unique_id),
-                wrap_string_with_quotes(node.name),
-                wrap_string_with_quotes(node.original_file_path | replace("\\","\\\\")),
-                wrap_string_with_quotes(node.alias),
-                wrap_string_with_quotes(node.resource_type),
-                wrap_string_with_quotes(node.source_name),
-                wrap_string_with_quotes(node.source_description),
-                wrap_string_with_quotes(node.description),
-                "cast(" ~ dbt_project_evaluator.is_not_empty_string(node.source_description) | trim ~ " as boolean)",
-                "cast(" ~ dbt_project_evaluator.is_not_empty_string(node.description) | trim ~ " as boolean)",
-                "cast(" ~ node.config.enabled ~ " as boolean)",
-                wrap_string_with_quotes(node.loaded_at_field | replace("'", "_")),
-                wrap_string_with_quotes(node.database),
-                wrap_string_with_quotes(node.schema),
-                wrap_string_with_quotes(node.package_name),
-                wrap_string_with_quotes(node.loader),
-                wrap_string_with_quotes(node.identifier),
-                wrap_string_with_quotes(node.tags | tojson),
-                wrap_string_with_quotes(node.freshness | tojson),
-                wrap_string_with_quotes(node.meta | tojson),
-                "cast(" ~ exclude_source ~ " as boolean)",
+             wrap_string_with_quotes(node.unique_id),
+             wrap_string_with_quotes(node.name),
+             wrap_string_with_quotes(node.original_file_path | replace("\\","\\\\")),
+             wrap_string_with_quotes(node.alias),
+             wrap_string_with_quotes(node.resource_type),
+             wrap_string_with_quotes(node.source_name),
+             wrap_string_with_quotes(node.source_description),
+             wrap_string_with_quotes(node.description),
+             "cast(" ~ dbt_project_evaluator.is_not_empty_string(node.source_description) | trim ~ " as boolean)",
+             "cast(" ~ dbt_project_evaluator.is_not_empty_string(node.description) | trim ~ " as boolean)",
+             "cast(" ~ node.config.enabled ~ " as boolean)",
+             wrap_string_with_quotes(node.loaded_at_field | replace("'", "_")),
+             wrap_string_with_quotes(node.database),
+             wrap_string_with_quotes(node.schema),
+             wrap_string_with_quotes(node.package_name),
+             wrap_string_with_quotes(node.loader),
+             wrap_string_with_quotes(node.identifier),
+             wrap_string_with_quotes(node.tags | tojson),
+             wrap_string_with_quotes(node.freshness | tojson),
+             wrap_string_with_quotes(node.meta | tojson),
+             "cast(" ~ exclude_source ~ " as boolean)",
             ]
         %}
             
@@ -45,5 +45,4 @@
 
 
     {{ return(values) }}
-
 {%- endmacro -%}
