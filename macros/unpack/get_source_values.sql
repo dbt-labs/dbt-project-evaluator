67ffd4878faf62eea2dd2cc52530ac54d1a3a322
Alexandra Fetterman <alexandra.fetterman@instructure.com>
2024-04-12 12:11:38 -0700

    Add source description and description to staging and macro

--- macros/unpack/get_source_values.sql
+++ macros/unpack/get_source_values.sql
@@ -12,25 +12,27 @@
 
         {%- set exclude_source = dbt_project_evaluator.set_is_excluded(node, resource_type="source") -%}
 
-         {%- set values_line = 
+        {%- set values_line = 
             [
-              wrap_string_with_quotes(node.unique_id),
-              wrap_string_with_quotes(node.name),
-              wrap_string_with_quotes(node.original_file_path | replace("\\","\\\\")),
-              wrap_string_with_quotes(node.alias),
-              wrap_string_with_quotes(node.resource_type),
-              wrap_string_with_quotes(node.source_name),
-              "cast(" ~ dbt_project_evaluator.is_not_empty_string(node.source_description) | trim ~ " as boolean)",
-              "cast(" ~ dbt_project_evaluator.is_not_empty_string(node.description) | trim ~ " as boolean)",
-              "cast(" ~ node.config.enabled ~ " as boolean)",
-              wrap_string_with_quotes(node.loaded_at_field | replace("'", "_")),
-              wrap_string_with_quotes(node.database),
-              wrap_string_with_quotes(node.schema),
-              wrap_string_with_quotes(node.package_name),
-              wrap_string_with_quotes(node.loader),
-              wrap_string_with_quotes(node.identifier),
-              wrap_string_with_quotes(node.meta | tojson),
-              "cast(" ~ exclude_source ~ " as boolean)",
+                wrap_string_with_quotes(node.unique_id),
+                wrap_string_with_quotes(node.name),
+                wrap_string_with_quotes(node.original_file_path | replace("\\","\\\\")),
+                wrap_string_with_quotes(node.alias),
+                wrap_string_with_quotes(node.resource_type),
+                wrap_string_with_quotes(node.source_name),
+                wrap_string_with_quotes(node.source_description),
+                wrap_string_with_quotes(node.description),
+                "cast(" ~ dbt_project_evaluator.is_not_empty_string(node.source_description) | trim ~ " as boolean)",
+                "cast(" ~ dbt_project_evaluator.is_not_empty_string(node.description) | trim ~ " as boolean)",
+                "cast(" ~ node.config.enabled ~ " as boolean)",
+                wrap_string_with_quotes(node.loaded_at_field | replace("'", "_")),
+                wrap_string_with_quotes(node.database),
+                wrap_string_with_quotes(node.schema),
+                wrap_string_with_quotes(node.package_name),
+                wrap_string_with_quotes(node.loader),
+                wrap_string_with_quotes(node.identifier),
+                wrap_string_with_quotes(node.meta | tojson),
+                "cast(" ~ exclude_source ~ " as boolean)",
             ]
         %}
             
@@ -41,5 +43,5 @@
 
 
     {{ return(values) }}
- 
+
 {%- endmacro -%}
--- models/staging/graph/stg_sources.sql
+++ models/staging/graph/stg_sources.sql
@@ -22,6 +22,8 @@ select
     cast(null as {{ dbt.type_string() }}) as alias,
     cast(null as {{ dbt.type_string() }}) as resource_type,
     cast(null as {{ dbt.type_string() }}) as source_name,
+    cast(null as {{ dbt.type_string() }}) as source_description,
+    cast(null as {{ dbt.type_string() }}) as description,
     cast(True as boolean) as is_source_described,
     cast(True as boolean) as is_described,
     cast(True as boolean) as is_enabled,
