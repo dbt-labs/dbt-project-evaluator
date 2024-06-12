508ba37a78084978458c000534b5f63b3c5a9451
jschintz-nytimes <167584907+jschintz-nytimes@users.noreply.github.com>
2024-06-12 12:50:07 -0400

    Create custom__incremental_strategy_is_insert_overwrite.sql

---
+++ tests/custom/incremental_logic/custom__incremental_strategy_is_insert_overwrite.sql
@@ -0,0 +1,27 @@
+{{ config(severity = env_var('DBT_PROJECT_EVALUATOR_SEVERITY', 'warn')) }}
+/*
+NYT has large volume & size datasets.
+Almost all models should have incremental models set to insert-overwrite strategy.
+Merging records is not performant over large datasets.  
+Append only is performant, but rarely useful.
+
+Incremental modelling makes my head hurt, but saves a lot of money $$$.  Read the docs again.
+https://docs.getdbt.com/docs/build/incremental-strategy
+*/
+select 
+    stg_nodes.*,
+    tables_metadata.row_count,
+    tables_metadata.size_bytes
+from {{ ref("stg_nodes") }}
+left outer join `{{ model.database }}.{{ model.schema }}.__TABLES__` as tables_metadata
+    on tables_metadata.project_id = stg_nodes.database
+    and tables_metadata.dataset_id = stg_nodes.schema
+    and tables_metadata.table_id = stg_nodes.alias -- use name instead?
+where
+    materialized = 'incremental'
+    and coalesce(incremental_strategy,'') not in ('append','insert_overwrite') 
+    -- only return large tables 1MM records or 50GB
+    and (
+        tables_metadata.row_count>999999
+        or tables_metadata.size_bytes > 4999999999
+    )
