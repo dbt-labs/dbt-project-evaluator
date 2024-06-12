829c6233f50ff8b1c7ad5f6693f5f2984fc549ef
jschintz-nytimes <167584907+jschintz-nytimes@users.noreply.github.com>
2024-06-12 12:45:53 -0400

    Create custom__column_description_is_singular_word.sql

---
+++ tests/custom/documentation/custom__column_description_is_singular_word.sql
@@ -0,0 +1,17 @@
+{{ config(severity=env_var("DBT_PROJECT_EVALUATOR_SEVERITY", "warn")) }}
+/*
+Every column description should be meaningful.
+Flag all columns that are single word
+*/
+select stg_columns.*
+from {{ ref("stg_columns") }}
+left outer join
+    {{ ref("stg_nodes") }} on stg_columns.node_unique_id = stg_nodes.unique_id
+where
+    -- Manually list centrally managed packages.  Ideally this would just
+    -- check for
+    -- current package name.
+    stg_nodes.package_name
+    not in ('dbt_project_evaluator', 'dig_dbt_sources', 'dig_dbt_utils')
+    -- counts how many spaces are present - after trimming to avoid trailing spaces
+    and array_length(split(trim(stg_columns.description), ' ')) < 2 and description != ''
