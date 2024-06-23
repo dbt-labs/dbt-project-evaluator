b0f6450726b58ee0e77a35f645beec9aa48af5f3
Igor Bolshakov <ibolsch@gmail.com>
2024-06-23 13:13:01 +0300

    move cte aliasing to a separate macro

--- macros/recursive_dag.sql
+++ macros/recursive_dag.sql
@@ -15,35 +15,8 @@ with recursive direct_relationships as (
 
 -- recursive CTE
 -- one record for every resource and each of its downstream children (including itself)
-all_relationships {% if target.name not in ['clickhouse'] %} (
-    parent_id,
-    parent,
-    parent_resource_type,
-    parent_model_type,
-    parent_materialized,
-    parent_access,
-    parent_is_public,
-    parent_source_name,
-    parent_file_path,
-    parent_directory_path,
-    parent_file_name,
-    parent_is_excluded,
-    child_id,
-    child,
-    child_resource_type,
-    child_model_type,
-    child_materialized,
-    child_access,
-    child_is_public,
-    child_source_name,
-    child_file_path,
-    child_directory_path,
-    child_file_name,
-    child_is_excluded,
-    distance,
-    path,
-    is_dependent_on_chain_of_views
-    ) {% endif %} as (
+all_relationships {{ recursive_dag__all_relationships_cte_aliases() }}
+    as (
     -- anchor
     select distinct
         resource_id as parent_id,
@@ -131,6 +104,41 @@ all_relationships {% if target.name not in ['clickhouse'] %} (
 
 )
 
+{% endmacro %}
+
+
+{% macro recursive_dag__all_relationships_cte_aliases() %}
+  {% if target.name not in ['clickhouse'] %} 
+    (
+      parent_id,
+      parent,
+      parent_resource_type,
+      parent_model_type,
+      parent_materialized,
+      parent_access,
+      parent_is_public,
+      parent_source_name,
+      parent_file_path,
+      parent_directory_path,
+      parent_file_name,
+      parent_is_excluded,
+      child_id,
+      child,
+      child_resource_type,
+      child_model_type,
+      child_materialized,
+      child_access,
+      child_is_public,
+      child_source_name,
+      child_file_path,
+      child_directory_path,
+      child_file_name,
+      child_is_excluded,
+      distance,
+      path,
+      is_dependent_on_chain_of_views
+    )
+  {% endif %}
 {% endmacro %}
 
 
